f789bc81443ae2b0e0499647c8c71e9961f63058
gatsuBerserk <berserkdev@outlook.it>
2022-03-28 15:04:38 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IndexHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexHeader from "@/components/IndexHeader.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IndexHeader,
   }
 }
 </script>
