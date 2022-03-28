b7a1c4c549fa58d2f37814d87983cfbcd981e7ba
gatsuBerserk <berserkdev@outlook.it>
2022-03-28 15:38:53 +0200

    header

--- src/components/IndexHeader.vue
+++ src/components/IndexHeader.vue
@@ -1,17 +1,100 @@
 <template>
-  <div>
-      Prova
+  <div> 
+      <div class="header">
+        <img src="../assets/img/dc-logo.png" alt=" Logo DS Comics">
+        <nav>
+            <ul>
+                <li v-for="(element, index) in manu" :key="index">
+                    <a href="#">{{ element.text }}</a>
+                </li>
+            </ul>
+        </nav>
+        </div>  
   </div>
 </template>
 
 <script>
-export default {
+export default { 
+    name : "IndexHeader", 
+    data: function(){
+        return{ 
+            manu :[
+                {
+                    text : "Characters", 
+                    url : "#" 
+                },
+                {
+                    text : "Comics", 
+                    url : "#" 
+                },
+                {
+                    text : "Movies", 
+                    url : "#" 
+                },
+                {
+                    text : "Tv", 
+                    url : "#" 
+                },
+                {
+                    text : "Games", 
+                    url : "#" 
+                },
+                {
+                    text : "Collectibles", 
+                    url : "#" 
+                },
+                {
+                    text : "Videos", 
+                    url : "#" 
+                },
+                {
+                    text : "Fans", 
+                    url : "#" 
+                },
+                {
+                    text : "News", 
+                    url : "#" 
+                },
+                {
+                    text : "Shop", 
+                    url : "#" 
+                },
+
+            ]
+        
+        }
+    }
 
 }
 </script>
 
 <style lang="scss" scoped>
 
+div.header{
+    display: flex; 
+    justify-content: space-around; 
+    align-items: center; 
 
+    ul{
+        display: flex;
+    }
+
+    li{
+        list-style: none; 
+        margin: 1rem; 
+        ;
+
+        :hover{
+            color: blue;
+        }
+    } 
+
+    a{
+        text-decoration:none; 
+        color: black;
+        text-transform: uppercase; 
+
+}
+}
 
 </style>
