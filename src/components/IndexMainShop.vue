309af3e373329331f329695b9349a1df2b0c6bc2
gatsuBerserk <berserkdev@outlook.it>
2022-03-29 15:39:46 +0200

    fix img

--- src/components/IndexMainShop.vue
+++ src/components/IndexMainShop.vue
@@ -3,11 +3,14 @@
         <div class="shop">
             <!--Appena finito cambiare con una lista e fare un v-for (ci ho pensato dopo mannaggia, meglio tardi che mai)--> 
             <!-- non funzionano png -->
-            <!-- <div class="shop-card" v-for="(element, index) in shopIcon" :key="index">
-                <img class="logos" :src="`../assets/img/` + element.shopImg" :alt="element.text"> 
-                <p> {{element.text}} </p>
-            </div> --> 
-            <div class="shop-cards">
+            <div class="shop-card" v-for="(element, index) in shopIcon" :key="index"> 
+                <a href="#"> 
+                    <img class="logos" :src="require(`@/assets/img/${element.shopImg}`)" :alt="element.text"> 
+                    {{element.text}}
+                </a>
+                
+            </div> 
+            <!-- <div class="shop-cards">
                 <img class="logos" src="../assets/img/buy-comics-digital-comics.png" alt="Digital Store"> 
                 <p> Digital Store</p> 
             </div>
@@ -26,7 +29,7 @@
             <div class="shop-cards">
                 <img class="logos" src="../assets/img/buy-dc-power-visa.svg" alt=" Digitl Store"> 
                 <a href="#"><p>power</p></a>
-            </div> 
+            </div>  -->
         </div>
     </div>
 </template>
@@ -34,43 +37,43 @@
 <script>
 export default { 
     name: "IndexMainShop",
-    // data : function(){
-    //     return{
-    //         shopIcon : [
-    //             {
-    //                 shopImg: "buy-comics-digital-comics.png", 
-    //                 name: "buy-comics-digital-comics", 
-    //                 text:"Digital Cimics"
+    data : function(){
+        return{
+            shopIcon : [
+                {
+                    shopImg: "buy-comics-digital-comics.png", 
+                    name: "buy-comics-digital-comics", 
+                    text:"Digital Cimics"
 
-    //             },
-    //             {
-    //                 shopImg: "buy-comics-merchandise.png", 
-    //                 name: "digital comics", 
-    //                 text:"DC merchandise"
+                },
+                {
+                    shopImg: "buy-comics-merchandise.png", 
+                    name: "digital comics", 
+                    text:"DC merchandise"
 
-    //             }, 
-    //             {
-    //                 shopImg: "buy-comics-subscriptions.png", 
-    //                 name: "Subscription", 
-    //                 text:"Subscription"
+                }, 
+                {
+                    shopImg: "buy-comics-subscriptions.png", 
+                    name: "Subscription", 
+                    text:"Subscription"
 
-    //             }, 
-    //             {
-    //                 shopImg: "buy-comics-digital-locator.png", 
-    //                 name: "digital locator", 
-    //                 text:"Comic shop locator"
+                }, 
+                {
+                    shopImg: "buy-comics-shop-locator.png", 
+                    name: "digital locator", 
+                    text:"Comic shop locator"
 
-    //             }, 
-    //             {
-    //                 shopImg: "buy-dc-power-visa.png", 
-    //                 name: "digital visa", 
-    //                 text:"Dc power Visa"
+                }, 
+                {
+                    shopImg: "buy-dc-power-visa.svg", 
+                    name: "digital visa", 
+                    text:"Dc power Visa"
 
-    //             } 
+                } 
 
-    //         ]
-    //     }
-    // }
+            ]
+        }
+    }
 
 }
 </script>
@@ -82,32 +85,38 @@ export default {
     }
     div.shop{ 
         @include container;
-        height: 100px; 
-         
-        // display: flex;
-        // align-items: center;
-        // justify-content: space-evenly;
-             
-            
-           
-    } 
-    
-    .shop-cards{ 
-        height: 100px;
+        height: 100px;  
+
+        .shop-card{ 
+        height: 100%;
         display: flex; 
         justify-content: space-evenly; 
         align-items: center;
-        padding: 0; 
+        padding: 0;
                 
             img.logos{
             height: 50px; 
+            margin-right: 1rem; 
             }
             
-            p{ 
-                margin-left: 1rem ;
-                display: inline-block;
+            a{ 
+                display: flex;
+                align-items: center;
+                margin-left: 1rem ; 
+                text-decoration: none; 
+                color: black; 
+                font-weight:bolder; 
+                text-transform: uppercase;
+                
+
+                &:hover{
+                    color: yellow;
+                }
                 } 
     }
+    } 
+    
+    
 
            
      
